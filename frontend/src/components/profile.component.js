9a3a52100d9681869e9f485d8215faf0320bb430
Itab-dev <62254808+Itab-dev@users.noreply.github.com>
2021-11-11 17:44:30 +0300

    Оформление профиля

--- frontend/src/components/profile.component.js
+++ frontend/src/components/profile.component.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import AuthService from "../services/auth.service";
-
+import Container from "react-bootstrap/Container";
+import {Link} from "react-router-dom";
 export default class Profile extends Component {
   constructor(props) {
     super(props);
@@ -26,31 +27,36 @@ export default class Profile extends Component {
     const { currentUser } = this.state;
 
     return (
-      <div className="container">
+      <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{height: window.innerHeight - 54}}>
         {(this.state.userReady) ?
-        <div>
-        <p>
+        <Card style={{width:400}} className="p-5">
+        <p1 align="center">
           Id{ ":"}
          <strong> {currentUser.id} </strong>
-        </p>
-          <p3>
+        </p1>
+          <p1>
             Username: <strong>{currentUser.username}</strong><br></br>
-          </p3>
+          </p1>
           <br></br>
           <p3>
-Balance: <strong>{currentUser.account}</strong><br></br>
-</p3>
-<p2>
-Contract{": "} <strong>{currentUser.contract}</strong><br></br>
-Sum to pay{": "} <strong>{currentUser.contract_sum}</strong><br></br>
-</p2>
-<br></br>
-        <p2>
-         Roles{": "}<br></br>
+          Balance: <strong>{currentUser.account}</strong><br></br>
+          </p3>
+          <p2>
+          Contract{": "} <strong>{currentUser.contract}</strong><br></br>
+          Sum to pay{": "} <strong>{currentUser.contract_sum}</strong><br></br>
+          </p2>
+          <br></br>
+          <p2>
+          Roles{": "}<br></br>
           {currentUser.roles && currentUser.roles.map((role, index) => <strong key={index}>{role}<br></br></strong>)}
-        </p2>
-      </div>: null}
-      </div>
+          </p2>
+          <div className="ml-auto" >
+            <Link to="/profile">История операций</Link>
+          </div>
+      </Card>: null}
+      </Container>
     );
   }
 }
