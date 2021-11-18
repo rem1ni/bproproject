11ff3f6c3d591b633ef688f8629bc40db2530e7b
tvoyaomereka <62567145+tvoyaomereka@users.noreply.github.com>
2021-11-18 18:16:20 +0300

    Добавление/удаление роли (back)

--- backend/src/main/java/com/backend/bprobackend/repository/RoleRepos.java
+++ backend/src/main/java/com/backend/bprobackend/repository/RoleRepos.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepos extends JpaRepository<Role,Long> {
+public interface RoleRepos extends JpaRepository<Role,Integer> {
     Optional<Role> findByName(EnumRole name);
 
 }
---
+++ backend/src/main/java/com/backend/bprobackend/request/RoleRequest.java
@@ -0,0 +1,22 @@
+package com.backend.bprobackend.request;
+
+public class RoleRequest {
+    private Long iduser;
+    private Integer idrole;
+
+    public Long getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(Long iduser) {
+        this.iduser = iduser;
+    }
+
+    public Integer getIdrole() {
+        return idrole;
+    }
+
+    public void setIdrole(Integer idrole) {
+        this.idrole = idrole;
+    }
+}
