4a5dad6e327fb885c159f8822b30ff66099a5dd5
Itab-dev <62254808+Itab-dev@users.noreply.github.com>
2021-11-19 14:50:37 +0300

    Update PostItem.jsx

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import AuthService from "../services/auth.service";
 const PostContract = (props) => {
+    const currentUser = AuthService.getCurrentUser();
+    let contract = currentUser.contract.id;
     return (
         <div className="post">
             <div className="post_content">
                 <strong>{props.post.id}. {props.post.name}</strong>
                 <div>{props.post.sum}</div>
+                (currentUser.contract.id === {props.post.id}) ?
+                <div>
+                    Выбран данный тариф
+                </div>
+                :
+                <button
+
+                >
+                    Выбрать
+                </button>
             </div>
         </div>
     );
