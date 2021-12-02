619ddea7a64df1882121fdc851c68fdf6727f75e
Itab-dev <62254808+Itab-dev@users.noreply.github.com>
2021-12-02 16:32:22 +0300

    Update Balance.jsx

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -35,7 +35,7 @@ const Balance = () => {
         <div className="container mt-5">
             <div className="col-md-4 col-md-offset-4  p-3 m-lg-auto">
                 Введите сумму пополнения:
-                <input className="input-range form-control mt-2" type="text" data-min="0" data-max="10000" placeholder="Сумма"
+                <input className="input-range form-control mt-2" type="text" required data-min="0" data-max="10000" placeholder="Сумма"
                        value={sum}
                        onChange={e => setSum(e.target.value)}
                 />
@@ -48,18 +48,18 @@ const Balance = () => {
 
                                 <div className="row ">
                                     <div className="col-md-12">
-                                        <input className="input-range form-control" type="text" data-min="0" data-max="9999999999999999" placeholder="Card Number" />
+                                        <input className="input-range form-control" type="text" required data-min="0" data-max="9999999999999999" placeholder="Card Number" />
                                     </div>
                                 </div>
 
                                 <div className="row ">
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <span className="help-block text-muted small-font"> Month</span>
-                                        <input className="input-range form-control" type="text" data-min="0" data-max="12" placeholder="MM"/>
+                                        <input className="input-range form-control" type="text" required data-min="0" data-max="12" placeholder="MM"/>
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <span className="help-block text-muted small-font"> Year</span>
-                                        <input className="input-range form-control" type="text" data-min="0" data-max="25" placeholder="YY"/>
+                                        <input className="input-range form-control" type="text" required data-min="0" data-max="25" placeholder="YY"/>
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <img src="https://bootstraptema.ru/snippets/form/2016/form-card/card.png"
@@ -67,14 +67,14 @@ const Balance = () => {
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <span className="help-block text-muted small-font"> CVV</span>
-                                        <input type="password" className="input-range form-control" data-min="0" data-max="999" placeholder="CVV"/>
+                                        <input type="password" className="input-range form-control" required data-min="0" data-max="999" placeholder="CVV"/>
                                     </div>
                                 </div>
 
                                 <div className="row mt-3">
                                     <div className="col-md-12 pad-adjust">
 
-                                        <input type="text" className="form-control" placeholder="Name"/>
+                                        <input type="text" required className="form-control" placeholder="Name"/>
                                     </div>
                                 </div>
 
@@ -90,8 +90,8 @@ const Balance = () => {
                             </div>
                         </div>
                     </div>
-                    {mes}
                 </div>
+                {mes}
             </div>
         </div>
     );
