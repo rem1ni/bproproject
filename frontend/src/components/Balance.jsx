584214028f2ab0d6dbbfb366b3539b6b187594a6
Itab-dev <62254808+Itab-dev@users.noreply.github.com>
2021-12-08 23:09:21 +0300

    Update Balance.jsx

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -2,43 +2,46 @@ import React,{useState} from 'react';
 import axios from "axios";
 import { Switch, Route, Link } from "react-router-dom";
 import AuthService from "../services/auth.service";
-import $ from 'jquery';
 const Balance = () => {
-    $('body').on('input', '.input-range', function(){
-        let value = this.value.replace(/[^0-9]/g, '');
-        if (value < $(this).data('min')) {
-            this.value = $(this).data('min');
-        } else if (value > $(this).data('max')) {
-            this.value = $(this).data('max');
-        } else {
-            this.value = value;
-        }
-    });
-    const[num,setNum]=useState();
-    const handleChange = (e) => {
-        setNum(e.target.value)
-    }
-    const [sum,setSum]=useState('');
+    const[num,setNum]=useState('');
+    const [m,setM]=useState('');
+    const [y,setY]=useState('');
+    const [c,setC]=useState('');
     const[mes,setMes] = useState("");
+    const handleChange = (e,set) => {
+        set(e.target.value)
+    }
+
+    const [sum,setSum]=useState('');
     const currentUser = AuthService.getCurrentUser();
     let iduser = currentUser.id;
     AuthService.ref(iduser);
     const cu = JSON.parse(localStorage.getItem('myKey'));
     let id=cu.account;
-    function Send() {
-        let num = Number(sum);
-        const currentUser = AuthService.getCurrentUser();
-        let i = currentUser.id;
+    const Send =(e)=> {
+        e.preventDefault()
+        if((num.toString().length) < 16){
+            setMes('Номер карты должен содержать 16 символов')
+        } else if(m.toString().length < 2){
+            setMes('Месяц должен содержать 2 символа')
+        }else if(y.toString().length < 2) {
+            setMes('Год должен содержать 2 символа')
+        }else if(c.toString().length < 3) {
+            setMes('CVV должен содержать 3 символа')
+        }else {
+            let num = Number(sum);
+            const currentUser = AuthService.getCurrentUser();
+            let i = currentUser.id;
 
-
-        return axios
-            .post("http://localhost:8080/bpro/userpay", {
-                i,
-                num
-            }).then(response => {
+            axios
+                .post("http://localhost:8080/bpro/userpay", {
+                    i,
+                    num
+                }).then(response => {
                 setMes(response.data);
 
             });
+        }
     }
     return (
         <div className="container mt-5">
@@ -47,9 +50,16 @@ const Balance = () => {
             <br></br>
             <br></br>
                 Введите сумму пополнения:
-                <input className="input-range form-control mt-2" type="text" required data-min="0" data-max="10000" placeholder="Сумма"
+                <input className="input-range form-control mt-2" type="number" required  placeholder="Сумма"
                        value={sum}
-                       onChange={e => setSum(e.target.value)}
+                       onChange={(e) => {
+                           if(e.target.value < 10000){
+                               handleChange(e,setSum)
+                           }else {
+                               e.target.value=  '10000'
+                               handleChange(e,setSum)
+                           }
+                       }}
                 />
             </div>
             <div className="row ">
@@ -57,34 +67,70 @@ const Balance = () => {
                     <div className="credit-card-div">
                         <div className="panel panel-default">
                             <div className="panel-heading">
-
+                            <form>
                                 <div className="row ">
                                     <div className="col-md-12">
                                         <input
-
+                                            className="input-range form-control"
                                             value={num}
                                             onChange={(e) => {
                                                 if(e.target.value.length <17){
-                                                    handleChange(e)
+                                                    handleChange(e,setNum)
                                                 }else {
                                                     e.target.value=  e.target.value.slice(0,2)
                                                 }
                                             }}
                                             type="number"
                                             placeholder={"0000 0000 0000 0000"}
-
+                                            required
                                         />
                                     </div>
                                 </div>
 
                                 <div className="row ">
-                                    <div className="col-md-3 col-sm-3 col-xs-3">
+                                    <div className="col-md-3 col-sm-3 col-xs-3 ">
                                         <span className="help-block text-muted small-font"> Месяц</span>
-                                        <input className="input-range form-control" type="text" required data-min="0" data-max="12" placeholder="ММ"/>
+                                        <input
+                                            className="input-range form-control"
+                                            value={m}
+                                            onChange={(e) => {
+                                                if(e.target.value.length <3){
+                                                    handleChange(e,setM)
+                                                    if(e.target.value>12){
+                                                        e.target.value='12'
+                                                        handleChange(e,setM)}
+                                                }
+                                                else
+                                                {
+                                                    e.target.value=  e.target.value.slice(0,2)
+                                                }
+                                            }}
+                                            type="number"
+                                            placeholder={"MM"}
+                                            required
+                                        />
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <span className="help-block text-muted small-font"> Год</span>
-                                        <input className="input-range form-control" type="text" required data-min="0" data-max="25" placeholder="ГГ"/>
+                                        <input
+                                            className="input-range form-control"
+                                            value={y}
+                                            onChange={(e) => {
+                                                if(e.target.value.length <3){
+                                                    handleChange(e,setY)
+                                                    if(e.target.value>26){
+                                                        e.target.value='26'
+                                                        handleChange(e,setY)}
+                                                }
+                                                else
+                                                {
+                                                    e.target.value=  e.target.value.slice(0,2)
+                                                }
+                                            }}
+                                            type="number"
+                                            placeholder={"ГГ"}
+                                            required
+                                        />
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <img src="https://bootstraptema.ru/snippets/form/2016/form-card/card.png"
@@ -92,20 +138,34 @@ const Balance = () => {
                                     </div>
                                     <div className="col-md-3 col-sm-3 col-xs-3">
                                         <span className="help-block text-muted small-font"> CVV</span>
-                                        <input type="password" className="input-range form-control" required data-min="0" data-max="999" placeholder="CVV"/>
+                                        <input
+                                            className="input-range form-control"
+                                            value={c}
+                                            onChange={(e) => {
+                                                if(e.target.value.length <4){
+                                                    handleChange(e,setC)
+                                                }
+                                                else
+                                                {
+                                                    e.target.value=  e.target.value.slice(0,2)
+                                                }
+                                            }}
+                                            type="password"
+                                            placeholder={"CVV"}
+                                            required
+                                        />
                                     </div>
                                 </div>
-                                
 
                                 <div className="row mt-3">
                                     <div className="col-md-6 col-sm-6 col-xs-6 pad-adjust">
-                                        <button onClick={() => {
-                                            Send(sum);
+                                        <button className="btn btn-success" onClick={
+                                            Send
                                         }
-                                        }>Оплатить</button>
+                                        >Оплатить</button>
                                     </div>
                                 </div>
-
+                            </form>
                             </div>
                         </div>
                     </div>
