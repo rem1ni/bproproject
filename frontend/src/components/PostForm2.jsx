04596eae15c2647ffae3d1ed170e7e3d6ec6f133
Ivan Taboyakov <taboyakov.ivan@mail.ru>
2022-05-20 16:42:53 +0300

    Update PostForm2.jsx

--- frontend/src/components/PostForm2.jsx
+++ frontend/src/components/PostForm2.jsx
@@ -29,7 +29,7 @@ const PostForm2 = ({create}) => {
             Admin
             <input type="checkbox" className=" form-check form-switch form-check-input" checked={checked2} onChange={handleChange2} />
             <br/>
-            <button onClick={addNewPost} className="btn btn-primary">Готово</button>
+            <button onClick={addNewPost} className="btn btn-primary">Select</button>
 
 
 
